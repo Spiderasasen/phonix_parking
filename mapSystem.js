4994bf4a682ce58891fee01cbac95040b7ccdbfd
spiderasasen <spiderasasen@gmail.com>
2026-05-03 11:03:39 -0400

    made small adjustments before i work on the account tab

--- mapSystem.js
+++ mapSystem.js
@@ -1,5 +1,6 @@
 // Create a real-world map centered on Elon University
 const map = L.map('map').setView([36.1025, -79.5069], 16);
+map.zoomControl.setPosition("bottomright");
 
 // Add OpenStreetMap tiles (free, no key needed)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
